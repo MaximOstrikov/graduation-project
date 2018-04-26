9a5122f9e33dfc3dd92d3ab5011d6e02c008a141
Unknown <AspixOne@gmail.com>
2018-04-27 00:18:49 +0300

    add button

--- frontend/js/proj.js
+++ frontend/js/proj.js
@@ -1,14 +1,16 @@
+// background slider
 var welcomeChecker = 1;
 function sliderBackgroundBody() {
     if (welcomeChecker === 1) {
-        setTimeout(function(){
-            $('#welcome').animate({opacity:1}, {duration:1000}).fadeOut(2000, function () {
+        setTimeout(function () {
+            $('#welcome').animate({opacity: 1}, {duration: 1000}).fadeOut(2000, function () {
                 $(this).remove()
             });
-        welcomeChecker++;
-        setTimeout(sliderBackgroundBody, 2200);},300);
+            welcomeChecker++;
+            setTimeout(sliderBackgroundBody, 2200);
+        }, 300);
         setTimeout(function () {
-            $('#task').animate({opacity:1}, {duration:1000}).removeClass('displayNone');
+            $('#task').animate({opacity: 1}, {duration: 1000}).removeClass('displayNone');
         }, 3600)
     } else {
         window.currBg = window.currBg + 1;
@@ -19,7 +21,28 @@ function sliderBackgroundBody() {
         setTimeout(sliderBackgroundBody, 15000);
     }
 }
-$(document).ready(function() {
+$(document).ready(function () {
     sliderBackgroundBody();
+});
+
+// add new task
+
+$('#addListBtn').click(function() {
+    var newTaskName = $('#addName').val(), newTaskDiscription = $('#addDiscription').val();
+    $('#addName, #addDiscription').val('');
+
+    $('#allTask').prepend('<div class="col-md-4 col-sm-6">\n' +
+    '             <div id="taskList">\n' +
+    '                 <div class="taskName"> <h3>'+ newTaskName +'</h3> </div>\n' +
+    '                 <div class="taskDiscription"> <p>'+ newTaskDiscription +'</p></div>\n' +
+    '                 <div class="rowBtn">\n' +
+    '                    <button id="edit">Edit</button>\n' +
+    '                    <button id="done">Done</button>\n' +
+    '                    <button id="remove">Remove</button>\n' +
+    '                 </div>\n' +
+    '             </div>\n' +
+    '         </div>');
+
+
 });
 
